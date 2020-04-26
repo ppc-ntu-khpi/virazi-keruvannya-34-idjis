952102977ae29f9c7becda7d607025de68becafd
idjis <63704562+idjis@users.noreply.github.com>
2020-04-26 18:38:37 +0300

    Update

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,24 @@
 package domain;
-
+/**
+ * Contains the implementation of the method
+ * @author IT-Service
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+	/**
+	 * A method containing the implementation of a given task
+	 * @param arr - parameter that is used to solve the problem
+	 * @param x - parameter that is used to solve the problem
+	 * @return n - return value
+	 */
+    public static int Calculate(int[] arr, int x){
+    	
+        int n=0;
+        
+        for(int temp:arr){
+            if(x==temp){
+                n++;
+            }
+        }
+        return n;
     }
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,35 @@
 package test;
 
 import domain.Exercise;
-
+import java.util.Arrays;
+/**
+ * Contains implementation of output of results
+ * @author IT-Service
+ */
 public class TestResult {
-
+	/**
+	 * The method contains creating an array and outputting its element according to the condition of the task
+	 * @param args - contains the arguments provided
+	 */
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+    	
+    	int[] arr = new int[10];
+        int n=0;
+        int reit=0;
+        
+    	for (int i = 0; i < arr.length; i++) {
+    	    arr[i] = ((int)(Math.random() * 10));
+    	}
+    	
+        System.out.println(Arrays.toString(arr));
+        
+        for(int temp:arr){
+            int num=Exercise.Calculate(arr, temp);
+            if(reit<num){
+                n=temp;
+                reit=num;
+            }
+        }
+        System.out.println("Найбільшу кількість разів повторюється число: " + n);
     }
 }
