02eeacfa6d3cb78645d8eb2399cfad5e1a40f103
idjis <63704562+idjis@users.noreply.github.com>
2020-04-26 19:03:11 +0300

    Update

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -7,15 +7,15 @@ public class Exercise {
 	/**
 	 * A method containing the implementation of a given task
 	 * @param arr - parameter that is used to solve the problem
-	 * @param x - parameter that is used to solve the problem
+	 * @param elem - parameter that is used to solve the problem
 	 * @return n - return value
 	 */
-    public static int Calculate(int[] arr, int x){
+    public static int Calculate(int[] arr, int elem){
     	
-        int n=0;
+    	int n=0;
         
         for(int temp:arr){
-            if(x==temp){
+            if(elem==temp){
                 n++;
             }
         }
