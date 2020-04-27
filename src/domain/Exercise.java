6055a16d1826a957294870aef1682856896a9e62
idjis <63704562+idjis@users.noreply.github.com>
2020-04-27 17:16:23 +0300

    Update

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,4 +1,6 @@
 package domain;
+
+import java.util.Arrays;
 /**
  * Contains the implementation of the method
  * @author IT-Service
@@ -7,18 +9,44 @@ public class Exercise {
 	/**
 	 * A method containing the implementation of a given task
 	 * @param array - parameter that is used to solve the problem
-	 * @param elem - parameter that is used to solve the problem
-	 * @return n - return value
+	 * @return reit - return value
 	 */
-    public static int Calculate(int[] array, int x){
-    	
-    	int number=0;
+	public static int Calculate(int[] array){
+		
+    	int reit = 0;
+        int number = 0;
+        int arr[] = new int[array.length];
+        int max = 0;
+        int amount = 0;
+
+        for (int x = 0; x < array.length; x++) {
+            for (int j = 0; j < array.length; j++){
+                if ((array[j] == array[x] ) && (j != x)){
+                	++number;
+                	}
+            }
+            arr[x] = number;
+            number = 1;
+        }
         
-        for(int temp:array){
-            if(x==temp){
-                number++;
+        for (int x = 0; x <= array.length-1; x++){
+            if (arr[x] > amount)
+            	amount = arr[x];
+        }
+        
+        System.out.println(Arrays.toString(array));
+        
+        for (int i = 0; i < array.length - 1; i++){
+            if (max <= arr[i+1]){
+                max = arr[i+1];}
+        }
+        
+        for (int i = 0; i < arr.length; i++){
+            if (max == arr[i]){
+                reit = array[i];
+                break;
             }
         }
-        return number;
+        return reit;
     }
-}
+ }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,35 +1,23 @@
 package test;
 
 import domain.Exercise;
-import java.util.Arrays;
 /**
  * Contains implementation of output of results
  * @author IT-Service
  */
-public class TestResult {
+public class TestResult extends Exercise{
 	/**
 	 * The method contains creating an array and outputting its element according to the condition of the task
 	 * @param args - contains the arguments provided
 	 */
     public static void main(String[] args) {
     	
-    	int[] array = new int[10];
-        int number=0;
-        int reit=0;
-        
-    	for (int i = 0; i < array.length; i++) {
-    	    array[i] = ((int)(Math.random() * 10));
-    	}
+    	int[] a = new int[10];
     	
-        System.out.println(Arrays.toString(array));
-        
-        for(int temp:array){
-            int num=Exercise.Calculate(array, temp);
-            if(reit<num){
-            	number=temp;
-                reit=num;
-            }
-        }
-        System.out.println("The largest number of times is a repeated number: " + number);
+    	for (int i = 0; i < a.length; i++) {
+    	    a[i] = ((int)(Math.random() * 10));
+    	}
+
+        System.out.println("Число которое чаще всего повторяется " + Exercise.Calculate(a));
     }
 }
